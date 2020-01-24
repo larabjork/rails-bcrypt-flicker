dd330786b1705b25c91442838d0de564d953d7f5
Student <example@example.com>
2020-01-23 16:28:09 -0800

    add styling to images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authorize, only: [:show]
-  before_action :admin_auth, only: [:edit]
+  before_action :authorize, only: [:show, :edit, :update]
+  before_action :admin_auth, only: [:destroy]
 
 
   # GET /users
